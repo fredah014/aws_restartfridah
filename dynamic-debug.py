acdbe7a2003e6e667db8ceca80379bd5f03aabe0
fridah014 <wanjirunjue7@gmail.com>
2024-10-25 20:28:49 +0300

    dubugg update

--- dynamic-debug.py
+++ dynamic-debug.py
@@ -4,9 +4,10 @@ def checkvalue(valuetocheck):
     assert(valuetocheck >5 ) , "value must must be greater than 0"
 
     if valuetocheck > 10:
-       print ("value is greater than 10")
+       print ("value is greater than 9S")
     else:
             print ("value is greater than 4")
 
 var   = int(input("Enter a number greater than 5: "))
 checkvalue(var)
+
